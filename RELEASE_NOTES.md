1959e2441376ddf913822ae7a0b4d439b9e99880
Flavio Lopes <flavio.lopes@ideiasfactory.tech>
2025-01-09 16:22:37 -0300

    Bugfixes 	Issue #2](https://github.com/dadosnapratica/language-code-table/issues/2) Solved 		- Correct Handling of Invalid URLs 		- Improved Logging for Invalid URLs
    Improvments
    	- Update to DataFrame Directly in Loop
    	- Base64 Encoding of Default Image
    	- Efficient URL Validation and Image Fetching
    	- Enhanced Error Handling and Default Image Fallback
    	- JSON Output Improvements
    	- Creation of util script get_base64_country_flag.py to place base64 string for images

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,31 @@
+# Release Notes - Program Updates
+
+2025-01-09 - Updates - tag v1.0.0
+-----
+**1. Correct Handling of Invalid URLs**
+
+*Summary:* Improved the handling of invalid URLs to ensure that all entries with non-valid URLs are updated to use a default image URL. This corrects issues where invalid URLs failed to display an alternative image.
+
+**2. Update to DataFrame Directly in Loop**
+
+*Summary*: Modified the process_country_flags function to directly update the DataFrame's 'country_flag_url' when an invalid URL is detected. Previously, changes to the URL during iteration did not persist post-function execution because the updates were made to a copy of the row rather than the DataFrame itself.
+
+**3. Improved Logging for Invalid URLs**
+
+*Summary*: Enhanced logging capabilities to provide more detailed feedback on processing steps, particularly around the handling of invalid URLs. This includes logging both when invalid URLs are detected and when default data is used as a replacement.
+
+**4. Base64 Encoding of Default Image**
+
+*Summary*: Ensured that the default image used for invalid URLs is correctly converted to Base64 format. This guarantees that the fallback image is properly displayed in all cases where the original URL fails to load.
+
+**5. Efficient URL Validation and Image Fetching**
+
+*Summary*: Streamlined the URL validation and image fetching processes to improve efficiency and reduce potential errors. The validate_url function now robustly checks URL validity and the fetching function handles various HTTP response scenarios gracefully.
+
+**6. Enhanced Error Handling and Default Image Fallback**
+
+*Summary*: Implemented more robust error handling within the fetch_and_convert_image function to manage exceptions more effectively. In cases of fetch failure, the function now reliably falls back to using a predefined default image.
+
+**7. JSON Output Improvements**
+
+*Summary*: Adjustments were made to ensure that JSON outputs are correctly formatted, particularly concerning the unescaping of URLs. This ensures that data saved in JSON format retains accurate and user-friendly URL strings.
--- get_base64_country_flag.py
+++ get_base64_country_flag.py
@@ -1,13 +1,68 @@
 import base64
 import sys
-#from sys import _ExitCode
 import requests
 import pandas as pd
 import json
+import validators
+import logging
+
+log_message_format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+# Configure the logging format
+logging.basicConfig(
+    level=logging.INFO,
+    format=log_message_format,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logger = logging.getLogger("get_base64_country_flag")
+default_image_data=None
+default_image_url='https://raw.githubusercontent.com/dadosnapratica/language-code-table/refs/heads/main/data/generic_flag.png'
+
+# Lista de colunas válidas
+valid_columns = [
+    'language_country_name',
+    'english_name',
+    'portuguese_br_name',
+    'native_name',
+    'subtag',
+    'country_flag_url',
+    'country_flag_base64'
+]
+
+def get_default_image_data():
+    global default_image_data
+    if default_image_data is None:
+        with open('./data/generic_flag.png', 'rb') as file:
+            default_image_data = file.read()
+            logger.info('First load Default Image')
+
+    return default_image_data    
+
+def remove_invalid_columns(data):
+    """
+    Remove columns from the DataFrame that are not in the list of valid columns.
+
+    Args:
+        data (pandas.DataFrame): The DataFrame to be filtered.
+        valid_columns (list): A list of strings representing the valid column names.
+
+    Returns:
+        pandas.DataFrame: The filtered DataFrame with only valid columns.
+    """
+    global valid_columns
+
+    # Filtrar o DataFrame para manter apenas as colunas válidas
+    filtered_data = data[valid_columns]
+    return filtered_data
+
+# Função para verificar se uma URL é válida
+def validate_url(url):
+    return url if validators.url(url) else None
 
 def fetch_and_convert_image(url):
     """
-    Fetches an image from a URL and converts it to a Base64 string.
+    Fetches an image from a URL and converts it to a Base64 string. If the fetch fails,
+    it uses a default image from a specified path.
     
     Args:
         url (str): The URL of the image.
@@ -16,39 +71,57 @@ def fetch_and_convert_image(url):
         str: The Base64 encoded string of the image.
     """
     try:
-        # Fetch the image from the URL
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an error for bad HTTP responses
+        # Attempt to fetch the image from the URL
+        response = requests.get(url)        
+        if response.status_code==200:
+            image_data = response.content
+        elif response.status_code==404:
+            logger.error(f'Url {url} not found.')
+            image_data=get_default_image_data()
+        else:
+            response.raise_for_status()  # Raise an error for bad HTTP responses             
 
-        # Encode the image to Base64
-        base64_image = base64.b64encode(response.content).decode('utf-8')
-        return base64_image
     except Exception as e:
-        print(f"Error fetching or converting image from {url}: {e}. Using default.")
-
-        return None
+        logger.exception(f"Error fetching or converting image from {url}", e)
+        #print(f"Error fetching or converting image from {url}: {e}")
+        # If an error occurs, use the default image        
+        image_data = get_default_image_data()
+    
+    # Encode the image data to Base64
+    base64_image = base64.b64encode(image_data).decode('utf-8')
+    return base64_image
 
 def process_country_flags(data):
     """
     Processes a list of country flag URLs and converts them to Base64.
+    Invalid URLs are replaced with None.
     
     Args:
-        data (list of dict): A list where each item is a dictionary containing 
-                             'country' and 'flag_url' keys.
-                             
+        data (pandas.DataFrame): A DataFrame where each row contains 'language_country_name' and 'country_flag_url'.
+    
     Returns:
-        dict: A dictionary mapping countries to their Base64-encoded flags.
+        dict: A dictionary mapping countries to their Base64-encoded flags or None for invalid URLs.
     """
+    global default_image_url
+
     base64_flags = {}
     for index, row in data.iterrows():
         country = row['language_country_name']
         url = row['country_flag_url']
-        print(f"Processing flag for {country}...")
-        base64_flags[country] = fetch_and_convert_image(url)
+        logger.debug(f"Processing flag for {country}...")
 
-    data['country_flag_base64']=data['language_country_name'].map(base64_flags)#pd.Series(base64_flags)
-    #print(data[['language_country_name','country_flag_base64']].head(10))
+        # Validate the URL
+        if validate_url(url)!=None:
+            base64_flags[country] = fetch_and_convert_image(url)
+        else:
+            logger.error(f"Invalid URL for {country}: {url}")
+            base64_flags[country] = base64.b64encode(get_default_image_data()).decode('utf-8')
+            # Update the DataFrame directly using .loc method for it to affect the original DataFrame.
+            data.loc[index, 'country_flag_url'] = default_image_url
+
+    data['country_flag_base64'] = data['language_country_name'].map(base64_flags)
     return base64_flags
+
 
 def save_json(data, csv_file_name):
     """
@@ -70,32 +143,78 @@ def save_json(data, csv_file_name):
 
     # Correcting the URLs by replacing escaped characters
     for item in data_json:
-        if 'country_flag_url' in item:
+        if 'country_flag_url' in item and item['country_flag_url'] != None:
             item['country_flag_url'] = item['country_flag_url'].replace('\\/', '/')
 
     # Write the updated JSON back to the file
     with open(json_path, 'w', encoding='utf-8') as file:
         json.dump(data_json, file, ensure_ascii=False, indent=4)
 
+def print_invalid_urls(flag_data):
+    # Applying URL validation and marking invalid ones
+    flag_data['is_url_valid'] = flag_data['country_flag_url'].apply(lambda url: validate_url(url) is not None)
+
+    # Filter rows where the URL is invalid
+    invalid_urls = flag_data[~flag_data['is_url_valid']]
+
+    # Logging the result
+    if not invalid_urls.empty:
+        logger.info('Invalid URLs found:')
+        logger.info(invalid_urls[['language_country_name', 'country_flag_url']])
+    else:
+        logger.info('No invalid URLs found.')
+
+    # Clean up by dropping the temporary validation column
+    flag_data.drop(columns=['is_url_valid'], inplace=True)
+
+def clean_data(flag_data):
+    # Aplicar a função para remover colunas inválidas
+    flag_data = remove_invalid_columns(flag_data)
+
+    # Verifica as Urls invalidas
+    print_invalid_urls(flag_data)    
+
+    # Aplica a validação em todas as URLs da coluna 'country_flag_url'
+    flag_data['country_flag_url'] = flag_data['country_flag_url'].apply(lambda x: validate_url(x))
+   
+    return flag_data
+
 # Example usage
 if __name__ == "__main__":
     csv_file_name='./data/dadosnapratica_language_code.csv'
     # Example data (replace with your actual data)
     flag_data = pd.read_csv(csv_file_name, encoding='utf-8')
+    flag_data.reset_index(drop=True, inplace=True)
     #flag_data.index.name='language_country_name'
-    print(flag_data.head())
-    #exit(0)
-    
+    logger.info('Original Table')    
+    logger.info(flag_data.head())
+
+    # Clean Original Data
+    flag_data=clean_data(flag_data)
+
     # Process the flags
     base64_flag_data = process_country_flags(flag_data)
+
+    # Logar as linhas onde a coluna 'country_flag_url' é None
+    # Check if 'country_flag_url' column contains the default_image_url
+    logger.info("Rows with default image URL:")
+    if 'country_flag_url' in flag_data.columns and default_image_url in flag_data['country_flag_url'].values:
+        count_invalid = len(flag_data[flag_data['country_flag_url'] == default_image_url])
+        logger.info(f'# Rows Invalid is {count_invalid}')
+        logger.info(flag_data[flag_data['country_flag_url'] == default_image_url].head())
+    else:
+        logger.info("No rows found with the default image URL.")
 
     # Output the results
     #for country, base64_flag in base64_flag_data.items():
     #    if base64_flag:
     #        print(f"Base64 flag for {country}:\n{base64_flag}...")  # Truncated for readability
     
-    print(flag_data.head(10))
-    flag_data.to_csv(csv_file_name, encoding='utf-8')
+    logger.info('Transformed Table')
+    flag_data.reset_index(drop=True, inplace=True)
+    logger.info(flag_data.head())    
+    flag_data.to_csv(csv_file_name, encoding='utf-8', index=False)
+    
     
     #flag_data.to_json(csv_file_name.replace('.csv','.json'), orient='records', force_ascii=False)
     save_json(flag_data, csv_file_name)
